1f100cdd49a0715f5ae4567c63d7ee03a009cf0b
VicRybakov <rybakov.vikt@yandex.ru>
2023-04-09 00:18:03 +0300

    немного пописал

---
+++ src/test/java/com/demoqa/FirstJUnitTest.java
@@ -0,0 +1,13 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTest {
+
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
